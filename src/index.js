405d57aca03ccd48e45089d9825945c8be595dd9
Jeremy Brown <jlbmagic@gmail.com>
2021-12-29 13:00:49 -0500

    removed extra code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import c3 from "c3";
-let chart;
 window.loadChart = (json) => {
   const obj = JSON.parse(json);
   const columns = obj.columns;
   const categories = obj.categories;
-  cats = categories;
   chart = c3.generate({
     bindto: "#chart",
     axis: { x: { type: "category", categories: categories } },
